a6959b7a4041c3ae68403b4826771626bbd64336
IoannisChris <148085717+IoannisChris@users.noreply.github.com>
2026-04-05 11:18:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import sea_level_predictor
+import time_series_visualizer
 from unittest import main
 
-# Test your function by calling it here
-sea_level_predictor.draw_plot()
+# Test your functions by calling them here
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
